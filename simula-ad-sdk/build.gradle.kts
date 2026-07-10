e19801983ccf4e51c0c4afbd37b5480cc7b9aef3
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2026-07-10 10:05:06 -0300

    New Release 1.1.3

--- simula-ad-sdk/build.gradle.kts
+++ simula-ad-sdk/build.gradle.kts
@@ -51,7 +51,7 @@ mavenPublishing {
     publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates("ad.simula", "ad-sdk", findProperty("VERSION_NAME")?.toString() ?: "1.1.2")
+    coordinates("ad.simula", "ad-sdk", findProperty("VERSION_NAME")?.toString() ?: "1.1.3")
 
     pom {
         name.set("Simula Ad SDK")
